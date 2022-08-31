e51b0f250ce2590bf19ae957c67e68e022639ba9
Sam Nayrouz <samuel.nayrouz@gmail.com>
2022-08-31 10:09:22 -0600

    Add collect operator

--- 03-transforming-operators/projects/Starter.playground/Contents.swift
+++ 03-transforming-operators/projects/Starter.playground/Contents.swift
@@ -5,6 +5,7 @@ var subscriptions = Set<AnyCancellable>()
 
 example(of: "collect") {
   ["A", "B", "C", "D", "E"].publisher
+        .collect()
     .sink(receiveCompletion: { print($0) },
           receiveValue: { print($0) })
     .store(in: &subscriptions)
